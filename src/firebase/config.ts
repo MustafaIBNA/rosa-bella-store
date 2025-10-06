0ed941302443aa7f2acb5e71a82be3a6b8de2a03
mostafa ashraf <mostafa.ashraf.dev@gmail.com>
2025-10-06 02:57:59 +0000

    Set up a Firebase backend

---
+++ src/firebase/config.ts
@@ -0,0 +1,8 @@
+export const firebaseConfig = {
+  "projectId": "studio-7065084476-f887b",
+  "appId": "1:76474858507:web:2bcda62bcb94475e315b0e",
+  "apiKey": "AIzaSyDogF0yOUZodVLHVoHCWvL3_ytyrYEyDgo",
+  "authDomain": "studio-7065084476-f887b.firebaseapp.com",
+  "measurementId": "",
+  "messagingSenderId": "76474858507"
+};
